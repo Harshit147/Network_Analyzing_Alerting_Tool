9e356a69a775fc0096e2e50e35cb23d82f12fbd4
Harshit <harshitparashar345@gmail.com>
2020-08-04 00:01:28 +0530

    Initial Commit

--- IDS/IDS/settings.py
+++ IDS/IDS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TCPIPPacketSniffer.apps.TcpippacketsnifferConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'IDS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['IDS/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
